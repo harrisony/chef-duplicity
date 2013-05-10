f01e20faac15a9983d359eb15da5da86b69be603
Harrison Conlin <me@harrisony.com>
2013-05-10 10:33:35 +1000

    Fix FC045

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name              "duplicity"
 maintainer        "Hector Castro"
 maintainer_email  "hectcastro@gmail.com"
 license           "Apache 2.0"
 description       "Installs and configures duplicity."
-version           "0.1.0"
+version           "0.1.1"
 recipe            "duplicity", "Installs and configures duplicity"
 
 %w{ build-essential python yum }.each do |d|
